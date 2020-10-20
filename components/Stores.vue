fe386736afe0d06468ea7041f8b15fdc28eff87e
vinuch <vincentedeh42@gmail.com>
2020-10-20 01:36:44 -0700

    .

--- components/Stores.vue
+++ components/Stores.vue
@@ -19,10 +19,14 @@
         Driving even more sales growth for your small business. 
       </p>
       <span class="text-simplifyYellow">Explore stores</span>
-      <div class="flex flex-wrap items-center  my-12">
+      <div class="flex flex-wrap items-center  my-16">
+          <!-- <input class="border-2 bg-white border-simplifyYellow px-4 w-full h-16 sm:w-9/12 z-50  sm:rounded-tl-lg sm:rounded-bl-lg my-" type="email" name="" id="" placeholder="Your email address"> -->
+          <button class="bg-simplifyYellow z-50 text-white px-4 py-4 rounded-lg my-2 w-full sm:w-4/12">Request early access</button>
+        </div>
+      <!-- <div class="flex flex-wrap items-center  my-12">
           <input class="border-2 bg-white border-simplifyYellow px-4 w-full h-16 sm:w-9/12 z-50  sm:rounded-tl-lg sm:rounded-bl-lg my-" type="email" name="" id="" placeholder="Your email address">
           <button class="bg-simplifyYellow z-50 text-white h-16 md:rounded-tr-lg my-2 md:rounded-br-lg w-full sm:w-3/12 ">Get Started</button>
-        </div>
+        </div> -->
     </div>
   </div>
 </template>
